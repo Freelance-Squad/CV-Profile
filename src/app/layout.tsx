ccf78f98e16fc9c312c3f219e03fd49188275462
Peter Ho <peterho@Peters-MacBook-Pro.local>
2023-02-01 06:09:08 +0800

    init with topbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,21 @@
-import './globals.css'
+'use client';
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+import React from 'react';
+import Topbar from '../components/Topbar';
+import './globals.css';
+
+const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <html lang="en">
+    <html lang='en'>
       {/*
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
+      <Topbar />
       <body>{children}</body>
     </html>
-  )
-}
+  );
+};
+
+export default RootLayout;
